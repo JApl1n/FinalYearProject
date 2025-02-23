1f0178bf0c591d654472e71fdcad03cafcd42012
kq21278@bristol.ac.uk <kq21278@bristol.ac.uk>
2025-02-23 16:24:33 +0000

    Added some analysis tools

--- project/MD/analyser.py
+++ project/MD/analyser.py
@@ -6,85 +6,14 @@ from PIL import Image
 import os
 
 import json
-from scipy.stats import gaussian_kde
-
-# orientational order parameter-  use it
-# second order legendre polynomial, find preferred direction 
-# Calculate entropy of a single frame
-def CalculateEntropy(positions, positionsGrid, shape):
-    kde = gaussian_kde(positions.T, bw_method = "scott")
-    
-    density = kde(positionsGrid).reshape(shape)
-
-    entropy = -density * np.log(density + 1e-10)
-    entropy = entropy / np.max(entropy)
-
-    return(entropy.ravel())
-
-
-
-# Make a trajectory png for a single frame in 3d
-def TrajFrame(positions, types, frameDir, timestep, colourmap, Lx, Ly, Lz, s2, allRodPositions):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
-
-    # Extract x, y, z positions
-    x = positions[:, 0]
-    y = positions[:, 1]
-    z = positions[:, 2]
-
-    normalisedTypes = types / np.max(types)  # Normalize tags to fit in the colormap range
-    
-    # Assign colors based on the normalized tags
-    colours = colourmap(normalisedTypes)
-    sizes = normalisedTypes*12 + 4
-
-
-    # Scatter plot for positions
-    ax.scatter(x, y, z, s=sizes, c=colours, marker="o")
-    for rodPositions in allRodPositions:
-        xRod, yRod, zRod = rodPositions
-
-        dx = np.abs(np.diff(xRod))
-        dy = np.abs(np.diff(yRod))
-        dz = np.abs(np.diff(zRod))
-
-        mask = (dx < Lx/2) & (dy < Ly/2) & (dz < Lz/2)
-        validIndices = np.where(mask)[0]
-
-        for idx in validIndices:
-            ax.plot(
-                    [xRod[idx], xRod[idx+1]],
-                    [yRod[idx], yRod[idx+1]],
-                    [zRod[idx], zRod[idx+1]],
-                    linewidth=2, color="black")
-
-        #ax.plot(rodPositions[0], rodPositions[1], rodPositions[2], linewidth=2, color="black")
-
-    # Set plot limits
-    ax.set_xlim([-Lx/2, Lx/2])
-    ax.set_ylim([-Ly/2, Ly/2])
-    ax.set_zlim([-Lz/2, Lz/2])
-
-    # Labels for the axes
-    ax.set_xlabel("X Position")
-    ax.set_ylabel("Y Position")
-    ax.set_zlabel("Z Position")
-
-    ax.set_title(f"Order Parameter: {s2}", size=10)
-
-    # Save the current plot as an image
-    frameFilename = os.path.join(frameDir, f"frame_{timestep}.png")
-    plt.savefig(frameFilename)
-    plt.close(fig)
-
-    return(frameFilename)
+from scipy.spatial.distance import pdist
 
 
 
 def ComputeOrderParameter(positions, numSolvent, numRods, rodLength):
     rodAxes = np.zeros((numRods, 3))
     allRodPositions = np.zeros((numRods, 3, rodLength))
+
     for rodNum in range(numRods):
         rodStartTag = numSolvent + rodNum * rodLength
         rodEndTag = rodStartTag + rodLength
@@ -93,16 +22,35 @@ def ComputeOrderParameter(positions, numSolvent, numRods, rodLength):
         allRodPositions[rodNum] = rodPositions.T
         rodAxis = rodPositions[-1] - rodPositions[0]
 
-        rodAxis /= np.linalg.norm(rodAxis)
+        rodAxis /= np.linalg.norm(rodAxis)  # Normalise rod axis
         rodAxes[rodNum] = rodAxis
 
+    # Compute director, mean rod direction
     director = np.mean(rodAxes, axis=0)
-    director /= np.linalg.norm(director)
+    director /= np.linalg.norm(director)  
     
-    cosThetaSquared = (rodAxes @ director) **2
-    s2 = np.round((3/2) * np.mean(cosThetaSquared) - 0.5, 6)
+    # Compute cos^2(theta) for each rod
+    cosThetaSquared = (rodAxes @ director) ** 2
 
-    return(s2, allRodPositions)
+    # Compute second order polynomial for each rod
+    P2Values = (3/2) * cosThetaSquared - 0.5
+
+    # Compute overall order parameter
+    S2 = np.round(np.mean(P2Values), 6)
+
+    return(S2, P2Values, allRodPositions)
+
+
+
+def ComputeMSD(allRodPositions):
+
+    rodParticles = allRodPositions.transpose(0,2,1).reshape(-1,3)
+    
+    distances = pdist(rodParticles, metric="euclidean")
+    msd = np.mean(distances**2)
+
+    return msd
+
 
 
 def ExtractData(metadataFilename, h5Filename):
@@ -119,40 +67,19 @@ def ExtractData(metadataFilename, h5Filename):
         params = json.load(f)
     f.close()
 
-    Lx, Ly, Lz = params["Lx"], params["Ly"], params["Lz"]
-    numSolvent = params["numSolvent"]
-    numRods = params["numRods"]
-    rodLength = params["rodLength"]
-    rodSpacing = params["rodSpacing"]
-
-    # Parameters editable here, but inherited initially from init.py
-    dt = params["dt"]  # Time step
-    dtWarmup = params["dtWarmup"]
-    drivingForceMagnitude = params["dtWarmup"] # Magnitude of force driving rods forward
-    warmupLength = params["warmupLength"]  # Number of timesteps to tune forces to prevent extreme initial velocities
-    simLength = params["simLength"]  # Number of timesteps for run of simulation
-    outStep = params["outStep"]  # Periodicity of output frames
-    kBond = params["kBond"]  # Strength of force between particles in rod
-    kAngle = params["kAngle"]  # Strength of force keeping particles in rod aligned
-    sigma = params["sigma"]  # Distance over which the leonard jones potential spreads
-    kT = params["kT"]  # Initial kinetic energy given to system after warmup
-    gammaSolvent = params["gammaSolvent"]  # Slight resistance given to solvent particles
-    gammaRod = params["gammaRod"]  # Slight resistance given to rod particles
-
-    ssei = params["ssei"]
-    rsei = params["rsei"]
-    rrei = params["rrei"]
-    ssef = params["ssef"]
-    rsef = params["rsef"]
-    rref = params["rref"]
-
     return timesteps, params
 
 
-def Animate(h5Filename, trajGifFilename, hmGifFilename, timesteps, params):
+
+def Iterate(h5Filename, trajGifFilename, hmGifFilename, timesteps, params):
     # Open the HDF5 file and read the positions
     f = h5py.File(h5Filename, "r")
     
+    msdValues = np.zeros(len(timesteps))
+    S2Values = np.zeros(len(timesteps))
+
+    index = 0
+
     for timestep in timesteps:
         # Extract the particle positions for this timestep
         dataset_name = f"step_{timestep}"
@@ -160,13 +87,38 @@ def Animate(h5Filename, trajGifFilename, hmGifFilename, timesteps, params):
 
         [numSolvent, numRods, rodLength] = params["numSolvent"], params["numRods"], params["rodLength"]
 
-        s2, allRodPositions = ComputeOrderParameter(positions, numSolvent, numRods, rodLength)
-        # Generatre frame for trajectory
-        #trajFilename = TrajFrame(positions, types, trajFrameDir, timestep, colourmap, Lx, Ly, Lz, s2, allRodPositions)
-        # Calculate entropy in 3d grid of the frame
-        #entropyAllFrames.append(CalculateEntropy(positions, positionsGrid, gridX.shape))
- 
-                
+        S2, P2Values = allRodPositions = ComputeOrderParameter(positions, numSolvent, numRods, rodLength)
+        msd = ComputeMSD(allRodPositions)
+
+        msdValues[index], S2Values[index] = msd, S2
+
+        index += 1
+
+    # Correlation tells if aigned rods move differently from unaligned
+    # If correlation > 0, more aligned rods move more, and move less if correlation < 0
+    correlation = np.corrcoef(msdValues, S2Values)[0,1]
+
+    return (msdValues, S2Values, correlation)
+
+
+
+def Plot(timesteps, msdValues, S2Values):
+
+    plt.plot(timesteps, msdValues, color="blue", alpha=0.6)
+    plt.xlabel("Timestep")
+    plt.ylabel("Mean Squared Displacement")
+    plt.title("MSD over time")
+    plt.show()
+    plt.savefig("MSD.png")
+    plt.close()
+
+    plt.plot(timesteps, S2Values, color="blue", alpha=0.6)
+    plt.xlabel("Timestep")
+    plt.ylabel("Order Parameter")
+    plt.title("Order Parameter over time")
+    plt.show()
+    plt.savefig("S2.png")
+
 
 
 def ViewLog(logFilename):
@@ -191,9 +143,14 @@ def main(metadataFilename, h5Filename, trajGifFilename, hmGifFilename, logFilena
 
     timesteps, params = ExtractData(metadataFilename, h5Filename)
 
-    Animate(h5Filename, trajGifFilename, hmGifFilename, timesteps, params)
+    msdValues, S2Values, correlation= Iterate(h5Filename, trajGifFilename, hmGifFilename, timesteps, params)
+
+    print(f"Correlation: {correlation}")
+
+    Plot(timesteps, msdValues, S2Values)
 
     ViewLog(logFilename)
+
 
 # Define input filenames
 metadataFilename = "simulationMetadata.json"
